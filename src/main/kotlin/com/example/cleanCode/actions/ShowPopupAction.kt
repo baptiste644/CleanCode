b4eb089ca6430c005b02b7eab4be51283202cc0a
baptiste <lefebvr.bat@gmail.com>
2024-10-19 14:28:18 +0200

    MAJ de l'interface

--- src/main/kotlin/com/example/cleanCode/actions/ShowPopupAction.kt
+++ src/main/kotlin/com/example/cleanCode/actions/ShowPopupAction.kt
@@ -4,6 +4,8 @@ import com.example.cleanCode.tasks.DialogInterfaceTask
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.ui.DialogWrapper
+import com.example.cleanCode.tasks.TestTask
+import com.example.cleanCode.tasks.endClean.EndCleanToExecuteTask
 
 class ShowPopupAction : AnAction() {
     override fun actionPerformed(e: AnActionEvent) {
@@ -13,6 +15,13 @@ class ShowPopupAction : AnAction() {
         if (dialog.exitCode == DialogWrapper.OK_EXIT_CODE) {
             val selected = dialog.getSelectedTask()
             println("Tâche sélectionnée : $selected")
+            if (selected == "end Cleaning") {
+                val checkBoxSelected = dialog.getSelectingCleaningTask()
+                EndCleanToExecuteTask().EndCleanToExecuteTask(e, checkBoxSelected)
+            }
+            if (selected === "test task") {
+                TestTask().testTask(e)
+            }
         }
     }
 }
